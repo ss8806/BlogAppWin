eb3e8a4b7408aa0fd6e81a2819d0930a6e78ecfc
sho <evo0622@gmail.com>
2024-07-18 21:07:07 +0900

    templates 変更

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     #TOP画面を表示する関数
-    return render(request, 'index.html')
+    # return render(request, 'index.html')
+    return render(request, 'blog/index.html')
