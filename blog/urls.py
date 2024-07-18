b70a3573e5d6bf3d02ad0718a4fd3b08b69c20b5
sho <evo0622@gmail.com>
2024-07-18 22:04:23 +0900

    関数ベース　クラスベース

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, IndexView
 
 urlpatterns = [
     path('index', index, name="index"),
-  
+    path('index_class', IndexView.as_view(), name="index_class"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 # Create your views here.
-def index(request):
-    #TOP画面を表示する関数
+
+
+def index(request):  # 関数ベース
+    # TOP画面を表示する関数
     # return render(request, 'index.html')
-    return render(request, 'blog/index.html')
+    print("index関数を使ってTOP画面を表示します！関数ベース")
+    return render(request, 'blog/index.html')  # 変更箇所
+
+
+class IndexView(TemplateView):  # クラスベース
+    # TOP画面を表示するクラス
+    template_name = 'blog/index.html'
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        print("IndexViewを使ってTOP画面を表示します！クラスベース")
+        return self.render_to_response(context)
