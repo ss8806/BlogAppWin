b92b7a2f0f6f75c466a705928fbf2e57fab5f55b
sho <evo0622@gmail.com>
2024-07-19 22:42:25 +0900

    一覧

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView  #追加
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView  # 追加
 from .models import Blog, Category
 
 # Create your views here.
@@ -21,9 +21,14 @@ class IndexView(TemplateView):  # クラスベース
         context = self.get_context_data(**kwargs)
         print("IndexViewを使ってTOP画面を表示します！クラスベース")
         return self.render_to_response(context)
-    
+
 
 class BlogListView(ListView):
     template_name = 'blog/blog_list.html'
     model = Blog
     queryset = Blog.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super(BlogListView, self).get_context_data(**kwargs)
+        context['category_list'] = Category.objects.all()
+        return context
